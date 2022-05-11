c274711163e13804d65833a602fba04544cd5bef
Alex-sqd <88975558+Alex-sqd@users.noreply.github.com>
2022-05-11 19:03:32 +0200

    Update break.py

--- break.py
+++ break.py
@@ -1,11 +1,12 @@
+""" break permite saltar al siguiente valor de x, evitando else, sin finalizar el bucle.
+    Si la condición de if no se cumple, no se ejecutan print ni break por lo que el programa continua  
+    la sentencia else y finaliza"""
+
 for n in range(2,10):
     for x in range(2, n):
         if n % x == 0:
             print(n, 'equals', x, '*', n//x)
             break
-    """ break permite saltar al siguiente valor de x, evitando else, sin finalizar el bucle.
-    Si la condición de if no se cumple, no se ejecutan print ni break por lo que el programa continua  
-    la sentencia else y finaliza"""
         else:
             # Si no se encuentra un multiplo
             print(n, 'is a prime number')
