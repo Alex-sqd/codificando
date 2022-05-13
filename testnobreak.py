c737b5f299b632e572ed1a450fff1edc151ea410
Alex-sqd <88975558+Alex-sqd@users.noreply.github.com>
2022-05-13 18:00:53 +0200

    Update testnobreak.py
    Cambios en el texto.

--- testnobreak.py
+++ testnobreak.py
@@ -1,8 +1,7 @@
 for n in range(2,10):
     for x in range(2, n):
         if n % x == 0:
-            print(n, 'equals', x, '*', n//x)
+            print(n, '=', x, '*', n//x)
     else:
-        # Loop fell through without finding a factor
         # Si no se encuentra un multiplo
-        print(n, 'is a prime number')
+        print(n, ' Es un número primo.')
