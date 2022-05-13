44d59ecb92527db79543f66a5f9ef8bae9294fb6
Alex-sqd <88975558+Alex-sqd@users.noreply.github.com>
2022-05-13 20:01:37 +0200

    Update logicOperators.py
    Table of AND and OR logic operations

--- logicOperators.py
+++ logicOperators.py
@@ -2,9 +2,6 @@
 Program to test and view de results of logic operations
 """
 
-A = (True, True, False, False)
-B = (True, False, True, False)
-
 def andOperator(ValA, ValB)
     andOp = ValA and ValB
     return andOp
@@ -13,7 +10,16 @@ def orOperator(ValA, ValB)
     orOp = ValA or ValB
     return orOp
 
-"""
-Falta hacer el bucle para que recorra los 4 valores de A
-con los 4 valores de B, con cada operación.
-"""
+def main():
+    bool = (True, False)
+    operator = ("and", "or")
+    for k in operator:
+        for i in bool:
+            for j in bool:
+                if k == "and":
+                    print(f"{j} and {i} is {andOperator(j,i)}")
+                else:
+                    print(f"{j} or {i} is {orOperator(j,i)}")
+
+if __name__ == '__main__':
+    main()
